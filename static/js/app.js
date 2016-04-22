9d7a93df667f8e0e224df54e4b80e495379f94d3
Akanksha Valecha <akanksha.valecha@gmail.com>
2016-04-22 16:21:05 +0530

    post, update, delete done

--- static/js/app.js
+++ static/js/app.js
@@ -16,10 +16,15 @@ app.directive('myEnter', function () {
 
 
 app.controller('discussionController',['$rootScope', '$scope', function($rootScope,$scope) { 
-	$scope.newComment = {};
-	$scope.level = 1;
-	$scope.newChildComment = {};
-	$scope.showReplyBox = false;
+	
+	$scope.reinit = function(){
+		$scope.newComment = {};
+		$scope.level = 1;
+		$scope.newChildComment = {};
+		$scope.updateInput = false;
+		$scope.showReplyBox = false;
+	}
+	$scope.reinit();
 	window.x = $scope;
 	$scope.discussionList = [
 	{
@@ -34,12 +39,12 @@ app.controller('discussionController',['$rootScope', '$scope', function($rootSco
 				comment: 'It looks like people are downvoting you based on what they assume you\'re saying and not what you\'re actually saying. Maybe they missed the last five words of your comment. Your point is something like; "Why isn\'t the title \'Books by earliest female writers...\'\", right?	'
 			},
 			{
-				name: 'yyyyy',
-				comment: 'There should be proper survey',
+				name: 'Lavish',
+				comment: 'I have no problem using female as in "female writer" I just think it\'s a little annoying when I see people use men and females in the same sentence, instead of men and women.',
 				comments: [
 				{
 					name: 'Ramajun',
-					comment: 'There were many such surverys'
+					comment: 'Feminist, checking in. Feminism\'s not a hive mind, so I\'m speaking for myself when I say: you\'re right about why "female" is sometimes cringe-worthy. When people use it as a weird, dehumanizing substitute for "woman" or just "person". Like, "The thing about females is..." or "You can\'t let a female..." But that\'s not this. OP could have said "female author", would have sounded fine. FWIW'
 				}
 				]
 			}
@@ -47,34 +52,45 @@ app.controller('discussionController',['$rootScope', '$scope', function($rootSco
 		},
 		{
 			name: 'Lata',
-			comment: 'blah blah blah',
+			comment: 'Saying men & females or women & males is like saying dogs & felines or cats & lines.',
 			comments: []
 		}
 		]
 	},
 	{
 		title: 'The cover of George Orwell\'s, "1984," becomes less censored with wear • /r/mildlyinteresting',
-		comments: []
+		comments: [
+		{
+			name: 'Krishna',
+			comment: 'We have noticed your thread\'s title mentioned a popular book title in /r/books. Please consider visiting some of these recent threads! You might also enjoy the subreddit /r/1984! I am a bot, and this action was performed automatically. Please contact the moderators of this subreddit if you have any questions or concerns.',
+			comments: []
+		}
+		]
 	}
 	]
+	$scope.saved = angular.fromJson(localStorage.getItem('discussionList'));
+	$scope.discussionList = (localStorage.getItem('discussionList'))!==null ? $scope.saved : $scope.discussionList;
+	localStorage.setItem('discussionList', angular.toJson($scope.discussionList));
 
 	$scope.addCommentDisc = function(disc, cc){
-		console.log(disc)
 		disc.comments.push({
 			name:cc.name,
 			comment:cc.comment
 		});
-		$scope.newComment = {};
-		$scope.newChildComment = {};
-		$scope.showReplyBox = false;
-		cc = {};
+		$scope.reinit();
+		localStorage.setItem('discussionList', angular.toJson($scope.discussionList));
 		return disc;
+	}
+
+	$scope.updateDiscussionList = function(disc){
+		$scope.reinit();
+		localStorage.setItem('discussionList', angular.toJson($scope.discussionList));
 	}
 
 }]);
 
 app.filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
+	return function(items) {
+		return items.slice().reverse();
+	};
 });
